5e32009fd58db1d9ab682c6cf1953ae16ce7e5a8
emilyjeong <merci_emily@naver.com>
2026-05-10 19:11:19 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // 이 파일은 URL만 들고 있어. index.html 갱신 시 건드릴 필요 X
 // ─────────────────────────────────────────────
 
-window.GAS_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycby5ewdTUM6bJJQERXkLgjodj8GSED7pHMww4hQ4pIFDJ4VMyVdXVrUIzYvNJwMc_U1q/exec';
