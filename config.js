8fd1a9c965fd0db641c97be5bc2a96d27d0472b3
emilyjeong <merci_emily@naver.com>
2026-05-11 22:01:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // 이 파일은 URL만 들고 있어. index.html 갱신 시 건드릴 필요 X
 // ─────────────────────────────────────────────
 
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycby5ewdTUM6bJJQERXkLgjodj8GSED7pHMww4hQ4pIFDJ4VMyVdXVrUIzYvNJwMc_U1q/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycby6Yqyt5bjnTM2XRO_hMY5W6AK6iwjULcY6H2UN4xnmyQ-Mx2Yho48iI2AWbyw6LKZ6/exec';
